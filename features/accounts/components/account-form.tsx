23cb0984e317eb9ac9976b3ad898dd2ed072f8e0
Ninjasri98 <palitsriparno@gmail.com>
2025-06-30 18:49:49 +0530

    added basic account form

---
+++ features/accounts/components/account-form.tsx
@@ -0,0 +1,70 @@
+import { insertAccountSchema } from "@/db/schema";
+import { useForm } from "react-hook-form";
+import z from "zod/v4";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { Form, FormControl, FormField, FormItem, FormLabel } from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+
+
+const formSchema = insertAccountSchema.pick({
+    name : true,
+});
+
+type FormValues = z.input<typeof formSchema>;
+
+type Props = {
+    id? : string;
+    defaultValues? : FormValues;
+    onSubmit : (values : FormValues) => void;
+    onDelete? : () => void;
+    disabled? : boolean;
+};
+
+export const AccountForm = ({
+    id,
+    defaultValues,
+    onSubmit,
+    onDelete,
+    disabled,
+} : Props) =>{
+    const form = useForm<FormValues>({
+        resolver: zodResolver(formSchema),
+        defaultValues : defaultValues
+    })
+
+    const handleSubmit = ( values : FormValues) =>{
+        console.log({values});
+    }
+
+    const handleDelete = () =>{
+        onDelete?.()
+    }
+
+    return (
+        <Form {...form} >
+            <form 
+            onSubmit={form.handleSubmit(handleSubmit)} 
+            className="space-y-4 pt-4">
+                <FormField 
+                name="name"
+                control={form.control}
+                render={({field}) => (
+                    <FormItem>
+                        <FormLabel>
+                            Name
+                        </FormLabel>
+                        <FormControl>
+                            <Input 
+                            disabled={disabled}
+                            placeholder="Account Name(Cash, Bank, Credit Card)"
+                            {...field} />
+                        </FormControl>
+                    </FormItem>
+                )}
+                />
+            </form>
+        </Form>
+    )
+}
+
+
--- features/accounts/components/new-account-sheet.tsx
+++ features/accounts/components/new-account-sheet.tsx
@@ -1,8 +1,13 @@
 import { Sheet, SheetContent, SheetDescription, SheetHeader, SheetTitle } from "@/components/ui/sheet"
+import { useNewAccount } from "../hooks/use-new-account"
+import { AccountForm } from "./account-form"
 
 export const NewAccountSheet = () =>{
+
+    const {isOpen,onClose} = useNewAccount()
+
     return(
-        <Sheet open>
+        <Sheet open = {isOpen} onOpenChange={onClose}>
             <SheetContent className="space-y-4">
                 <SheetHeader>
                     <SheetTitle>
@@ -12,6 +17,7 @@ export const NewAccountSheet = () =>{
                         Create a new account to track your transactions.
                     </SheetDescription>
                 </SheetHeader>
+                <AccountForm onSubmit={() => {}} disabled={false} />
             </SheetContent>
         </Sheet>
     )
