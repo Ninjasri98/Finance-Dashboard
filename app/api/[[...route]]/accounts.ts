8a968081ee777b18301c0e33598f154385bb6fcd
Ninjasri98 <palitsriparno@gmail.com>
2025-06-30 09:32:35 +0530

    created accounts POST endpoint

--- app/api/[[...route]]/accounts.ts
+++ app/api/[[...route]]/accounts.ts
@@ -1,9 +1,10 @@
 import { db } from "@/db/drizzle";
-import { accounts } from "@/db/schema";
+import { accounts, insertAccountSchema } from "@/db/schema";
 import { clerkMiddleware, getAuth } from "@hono/clerk-auth";
 import { eq } from "drizzle-orm";
 import { Hono } from "hono";
-
+import {zValidator} from "@hono/zod-validator";
+import { createId } from "@paralleldrive/cuid2";
 
 const app = new Hono().get("/",
     clerkMiddleware()
@@ -25,5 +26,27 @@ const app = new Hono().get("/",
 
         return c.json({ data })
     })
+    .post("/",
+        clerkMiddleware(),
+        zValidator("json",insertAccountSchema.pick({
+            name : true,
+        })),
+        async (c) => {
+            const auth = getAuth(c);
+            const values = c.req.valid("json");
+
+            if (!auth?.userId) {
+                return c.json({ error: "Unauthorized" }, 401);
+            }
+
+            const [data] = await db.insert(accounts).values({
+                id : createId(),
+                userId : auth.userId,
+                ...values
+            }).returning()
+
+            return c.json({ data })
+        }
+    )
 
 export default app;
