2ebf1d7ffb58685fa16b47f064a55446920fc8af
Ninjasri98 <palitsriparno@gmail.com>
2025-07-01 18:58:24 +0530

    added pagination , sorting, filtering and other functionalities to data table

--- app/(dashboard)/accounts/page.tsx
+++ app/(dashboard)/accounts/page.tsx
@@ -1,10 +1,28 @@
 "use client";
 
 import { Button } from '@/components/ui/button'
-import { Card, CardHeader, CardTitle } from '@/components/ui/card'
+import { Card, CardContent, CardHeader, CardTitle } from '@/components/ui/card'
 import { useNewAccount } from '@/features/accounts/hooks/use-new-account'
 import { Plus } from 'lucide-react'
 import React from 'react'
+import { columns, Payment } from './columns';
+import { DataTable } from '@/components/data-table';
+
+
+const data : Payment[] = [
+    {
+      id: "728ed52f",
+      amount: 100,
+      status: "pending",
+      email: "m@example.com",
+    },
+     {
+      id: "728ed52f",
+      amount: 100,
+      status: "success",
+      email: "a@example.com",
+    },
+]
 
 const AccountsPage = () => {
 
@@ -21,6 +39,9 @@ const AccountsPage = () => {
                         Add New
                     </Button>
                 </CardHeader>
+                <CardContent>
+                    <DataTable columns={columns} data={data} filterKey='email' onDelete={() => {}} />
+                </CardContent>
             </Card>
         </div>
     )
