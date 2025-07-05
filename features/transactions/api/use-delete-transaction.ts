d4a9a30de0fd562c442116153576dd676e2c9c29
Ninjasri98 <palitsriparno@gmail.com>
2025-07-05 10:40:34 +0530

    modified api endpoints for transactions

---
+++ features/transactions/api/use-delete-transaction.ts
@@ -0,0 +1,30 @@
+import {  InferResponseType } from "hono";
+import { client } from "@/lib/hono";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
+import { toast } from "sonner";
+
+type ResponseType = InferResponseType<typeof client.api.transactions[":id"]["$delete"]>;
+
+export const useDeletetTransaction = (id? : string) =>{
+    const queryClient = useQueryClient();
+
+    const mutation = useMutation<ResponseType, Error>({
+        mutationFn: async() =>{
+            const response = await client.api.transactions[":id"]["$delete"]({
+                param : {id}
+            })
+
+            return await response.json();
+        },
+        onSuccess : () =>{
+            toast.success("Transaction Deleted");
+            queryClient.invalidateQueries({queryKey: ["transaction", {id}]});
+            queryClient.invalidateQueries({queryKey: ["transactions"]});
+        },
+        onError : () =>{
+            toast.error("Failed to delete transaction");
+        }
+    })
+
+    return mutation;
+}
