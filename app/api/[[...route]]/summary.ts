281907f4bb5a022e35cc39cb12cb748fb42732b1
Ninjasri98 <palitsriparno@gmail.com>
2025-07-10 09:41:53 +0530

    added function for fetching top categories

--- app/api/[[...route]]/summary.ts
+++ app/api/[[...route]]/summary.ts
@@ -1,12 +1,13 @@
 import { db } from "@/db/drizzle";
-import { accounts, transactions } from "@/db/schema";
+import { accounts, categories, transactions } from "@/db/schema";
 import { clerkMiddleware, getAuth } from "@hono/clerk-auth";
 import { zValidator } from "@hono/zod-validator";
 import { differenceInDays, parse, subDays } from "date-fns";
-import { and, eq, gte, lte, sql, sum } from "drizzle-orm";
+import { and, eq, gte, lt, lte, sql, sum, desc } from "drizzle-orm";
 import { Hono } from "hono";
 import z from "zod/v4";
 import { HTTPException } from "hono/http-exception";
+import { calculatePercentage } from "@/lib/utils";
 
 const app = new Hono().get(
     "/",
@@ -65,6 +66,58 @@ const app = new Hono().get(
                         lte(transactions.date, endDate)
                     )
                 );
+        }
+
+        const [currentPeriod] = await fetchFinancialData(
+            auth.userId,
+            startDate,
+            endDate
+        );
+        const [lastPeriod] = await fetchFinancialData(
+            auth.userId,
+            lastPeriodStart,
+            lastPeriodEnd
+        );
+
+        const incomeChange = calculatePercentage(
+            currentPeriod.income ?? 0,
+            lastPeriod.income ?? 0
+        );
+        const expensesChange = calculatePercentage(
+            currentPeriod.expenses ?? 0,
+            lastPeriod.expenses ?? 0
+        );
+        const remainingChange = calculatePercentage(
+            currentPeriod.remaining ?? 0,
+            lastPeriod.remaining ?? 0
+        );
+
+        const category = await db
+            .select({
+                name: categories.name,
+                value: sql`SUM(ABS(${transactions.amount}))`.mapWith(Number),
+            })
+            .from(transactions)
+            .innerJoin(accounts, eq(transactions.accountId, accounts.id))
+            .innerJoin(categories, eq(transactions.categoryId, categories.id))
+            .where(
+                and(
+                    accountId ? eq(accounts.id, accountId) : undefined,
+                    eq(accounts.userId, auth.userId),
+                    lt(transactions.amount, 0),
+                    gte(transactions.date, startDate),
+                    lte(transactions.date, endDate)
+                )
+            )
+            .groupBy(categories.name)
+            .orderBy(desc(sql`SUM(ABS(${transactions.amount}))`.mapWith(Number)));
+
+        const topCategories = category.slice(0, 3);
+        const otherCategories = category.slice(3);
+        const otherSum = otherCategories.reduce((acc, curr) => acc + curr.value, 0);
+        const finalCategories = topCategories;
+        if (otherCategories.length > 0) {
+            finalCategories.push({ name: "Other", value: otherSum });
         }
     }
 );
